d76e0aeba27ee607b72b7f0a00b9c9ac4487c074
Dominik Grzedzielski <gie.dominik@gmail.com>
2023-11-03 17:15:36 +0100

    test broken e2e

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <div className="max-w-xl text-3xl">
-        This is NEXT <span className="text-gray-500 line-through">13</span>{' '}
+        This is BUG <span className="text-gray-500 line-through">13</span>{' '}
         <span className="font-bold">14</span> Template with configured Linting,
         Storybook, Tailwind, shadcn/ui
       </div>
