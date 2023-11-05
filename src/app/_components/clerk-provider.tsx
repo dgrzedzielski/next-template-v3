fbc7d9fcd75fa73f529126412b3744207b84a54a
Dominik Grzedzielski <gie.dominik@gmail.com>
2023-11-05 16:15:53 +0100

    chore: add multitheme support

--- src/app/_components/clerk-provider.tsx
+++ src/app/_components/clerk-provider.tsx
@@ -1,6 +1,8 @@
 'use client';
 import * as React from 'react';
 
+import { useTheme } from 'next-themes';
+
 import { ClerkProvider as BaseClerkProvider } from '@clerk/nextjs';
 import { dark } from '@clerk/themes';
 
@@ -10,14 +12,14 @@ import { Theme } from '@/styles/themes/type';
 import { inputClassNames } from '@/ui/input';
 
 export const ClerkProvider = ({ children }: { children: React.ReactNode }) => {
-  const currentTheme = 'dark'; // TODO: make it dynamic for supporting light/dark mode
+  const { resolvedTheme: currentTheme } = useTheme();
 
   const theme: Theme = currentTheme === 'dark' ? darkTheme : lightTheme;
 
   return (
     <BaseClerkProvider
       appearance={{
-        baseTheme: dark,
+        baseTheme: currentTheme === 'dark' ? dark : undefined,
         variables: {
           colorPrimary: `hsl(${theme.primary})`,
           colorText: `hsl(${theme.foreground})`,
